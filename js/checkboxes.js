6f2cedd636007df1ee5ba25446cbebdf311c45f9
tymofii-s <t967887744@gmail.com>
2025-01-26 21:56:52 +0200

    add repeating song && footer pocket for player && sticky filters block

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -1,6 +1,17 @@
+const audio = document.querySelector('audio');
+const repeatCheckbox = document.getElementById('repeatSong');
+
+repeatCheckbox.addEventListener('change', function() {
+    audio.loop = this.checked; // Set the audio loop property based on checkbox state
+});
+
 // Додаємо обробник події для закінчення пісні
 player.onended = () => {
-    if (autoQueue) playNext();
+    if (repeatCheckbox.checked) {
+        playSong(currentIndex); // Повторюємо пісню, якщо галочка активна
+    } else if (autoQueue) {
+        playNext();
+    }
 };
 
 // Функції для фільтрів
